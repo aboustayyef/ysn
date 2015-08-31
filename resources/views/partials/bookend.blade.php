c3c9f1e6da06720fa40b33e7df0fe3f3b6f4e6de
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-08-31 06:24:51 +0000

    small adjustments to bookend

--- resources/views/partials/bookend.blade.php
+++ resources/views/partials/bookend.blade.php
@@ -1,6 +1,7 @@
 <div class="cardsWrapper">
 	<div class="card green">
 		<h3>Want to see more posts?</h3>
-		<h4><a href="/">refresh this page</a></h4>
+		<p>This website gathers new content every five minutes</p>
+		<h4><a href="/">Click to refresh</a></h4>
 	</div>
 </div>
