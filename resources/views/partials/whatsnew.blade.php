71a242b3c5b7f77802c9cd68e82f799e15c200fd
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-09-09 06:42:51 +0000

    removed "new on" and focused on spread the word

--- resources/views/partials/whatsnew.blade.php
+++ resources/views/partials/whatsnew.blade.php
@@ -1,15 +1,7 @@
 <div class="cardsWrapper">
 	<div id="whatsnew" class="card">
-		<h3>New on youstink.news</h3>
-		<ul>
-			<li><strong>Added Facebook</strong></li>
-			<li><strong>Quality Control:</strong> Less Spammy Posts</li>
-			<li><strong>Keep us in the background:</strong> You'll be notified of new posts</li>
-			<li><strong>One stream</strong> for all sources</li>
-			<li><strong>Channel Browsing:</strong> You can browse sources individually</li>
-			<li><strong>Visual facelift</strong></li>
-			<li><strong>Bug Fixes</strong></li>
-		</ul>
+		{{-- <h3>New on youstink.news</h3> --}}
+		<p>Do you find this website useful?</p>
 		<a href="https://twitter.com/intent/tweet?text=Check+out+youstink.news+to+keep+a+tab+on+all+the+%23youstink+action+in+%23Lebanon" class="button">
 			Spread the Word
 		</a>
